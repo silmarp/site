d592af6681ed6589c492b78309fc6b39ac682e68
Silmar <silmarjr2@gmail.com>
2023-09-26 17:47:37 -0300

    fix flake build nix build couldn't find hugo theme, adding the theme as an input solves the issue

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    terminal = {
+      url = "github:panr/hugo-theme-terminal/2b14b3d4e5eab53aa45647490bb797b642a82a59";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, utils }: 
+  outputs = { self, nixpkgs, utils, terminal }: 
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        themeName = ((builtins.fromTOML (builtins.readFile "${terminal}/theme.toml")).name);
       in
       {
         packages.website = pkgs.stdenv.mkDerivation rec {
@@ -17,6 +22,10 @@
           version = "2023-03-20";
           src = ./src/.;
           nativeBuildInputs = [ pkgs.hugo ];
+          configurePhase = ''
+            mkdir -p "themes/terminal"
+            cp -r ${terminal}/* "themes/${themeName}"
+          '';
           buildPhase = "hugo";
           installPhase = "cp -r public $out";
         };
